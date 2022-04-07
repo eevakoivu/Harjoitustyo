346d21a8118753f80528714e98845c57da2f1266
Anna <lindholmannaeina@gmail.com>
2022-04-07 13:43:40 +0300

    kahdella nappulalla

--- app/src/main/java/com/example/testi/MainActivity.java
+++ app/src/main/java/com/example/testi/MainActivity.java
@@ -8,11 +8,13 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.nappula);
+        button2 =(Button) findViewById(R.id.nappula2);
     }
 }
