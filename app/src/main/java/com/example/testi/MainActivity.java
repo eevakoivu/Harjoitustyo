30966da2d1b21d133db45eef115d5ae613f4ddde
eevakoivu <eevamaria.koivu@gmail.com>
2022-04-07 13:31:44 +0300

    intial commit

---
+++ app/src/main/java/com/example/testi/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.testi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.nappula);
+    }
+}
